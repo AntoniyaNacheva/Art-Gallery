3b164b2e2108fa9e6d2c91c568f45d89b76771a0
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-16 12:55:16 +0200

    Add Edit page

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -2,12 +2,43 @@ const router = require('express').Router();
 
 const { isAuth } = require('../middlewares/authMiddleware');
 const publicationService = require('../services/publicationService');
+const { preloadPublication, isPublicationAuthor } = require('../middlewares/publicationMiddlewares');
+const { getErrorMessage } = require('../utils/errorHelpers');
 
 router.get('/', async (req, res) => {
 	const publications = await publicationService.getAll().lean();
 
 	res.render('publication', { publications });
 });
+
+router.get('/:publicationId/details', async (req, res) => {
+	const publication = await publicationService.getOneDetailed(req.params.publicationId).lean();
+	const isAuthor = publication.author._id == req.user?._id;
+
+	res.render('publication/details', { ...publication, isAuthor });
+});
+
+router.get('/:publicationId/edit',
+	isAuth, preloadPublication, isPublicationAuthor,
+	(req, res) => {
+		res.render('publication/edit', { ...req.publication });
+	}
+);
+
+router.post('/:publicationId/edit',
+	isAuth, preloadPublication, isPublicationAuthor,
+	async (req, res) => {
+
+		try {
+			await publicationService.updateOne(req.params.publicationId, req.body);
+
+			res.redirect(`/publications/${req.params.publicationId}/details`);
+
+		} catch (error) {
+			res.render('publication/edit', { ...req.body, error: getErrorMessage(error) });
+		}
+	}
+);
 
 router.get('/create', isAuth, (req, res) => {
 	res.render('publication/create');
@@ -16,9 +47,15 @@ router.get('/create', isAuth, (req, res) => {
 router.post('/create', isAuth, async (req, res) => {
 	const publicationData = { ...req.body, author: req.user._id };
 
-	await publicationService.create(publicationData);
+	try {
+		await publicationService.create(publicationData);
 
-	res.redirect('/publications');
+		res.redirect('/publications');
+
+	} catch (error) {
+
+		res.render('publication/create', { ...req.body, error: getErrorMessage(error) });
+	}
 });
 
 module.exports = router;
--- services/publicationService.js
+++ services/publicationService.js
@@ -2,5 +2,10 @@ const Publication = require('../models/Publication');
 
 exports.getAll = () => Publication.find();
 
-exports.create = (publicationData) =>
-	Publication.create(publicationData);
+exports.getOne = (publicationId) => Publication.findById(publicationId);
+
+exports.getOneDetailed = (publicationId) => Publication.findById(publicationId).populate('author');
+
+exports.create = (publicationData) => Publication.create(publicationData);
+
+exports.update = (publicationId, publicationData) => Publication.updateOne({ _id: publicationId }, { $set: publicationData }, {runValidators: true});
